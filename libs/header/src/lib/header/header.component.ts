bd00c1fde1d31da45c81371bc5200e81d1e9f958
unknown <sridharswaminath@SEA0296L.seargindomain.local>
2025-03-11 16:23:00 +0800

    updated

---
+++ libs/header/src/lib/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
